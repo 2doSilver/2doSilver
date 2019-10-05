8b45040baedddc5d494c5b2cbc76869ae20c355f
2doSilver <2dosilver@gmail.com>
2019-10-05 23:20:30 +0900

    [toLowerCase] to Lower Case 1차 시도

---
+++ leetCode/sol_toLowerCase.java
@@ -0,0 +1,11 @@
+package leetCode;
+
+public class sol_toLowerCase {
+	public static void main(String args[]) {
+		System.out.println(toLowerCase("Hello"));
+	}
+	
+    public static String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
